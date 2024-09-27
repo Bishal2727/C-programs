37e71f58410371a11990333a8af12a8695c6fbc0
Bishal2727 <37079537+Bishal2727@users.noreply.github.com>
2024-09-27 12:09:45 +0530

    factorial_userDefined.c

--- factorial_userDefined.c
+++ factorial_userDefined.c
@@ -16,12 +16,6 @@ int main() {
 
     printf("Enter a positive integer: ");
     scanf("%d", &num);
-
-    if (num < 0) {
-        printf("Factorial is not defined for negative numbers.\n");
-    } else {
-        printf("Factorial of %d is: %d\n", num, factorial(num));
-    }
-
+    printf("Factorial of %d is: %d\n", num, factorial(num));
     return 0;
 }
