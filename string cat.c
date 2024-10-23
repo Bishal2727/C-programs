4f2436337152d6f2f6e351e23d1aa4e35586cb61
Bishal2727 <37079537+Bishal2727@users.noreply.github.com>
2024-10-24 01:43:41 +0530

    Add files via upload

---
+++ string cat.c
@@ -0,0 +1,16 @@
+//String concatenation
+
+#include<stdio.h>
+#include<string.h>
+main()
+{
+char fname[25],cname[25];
+printf("Enter the string:\n");
+scanf("%s",fname);
+printf("Enter the second string:\n");
+scanf("%s",cname);
+strcat(fname, cname);
+printf("%s",fname);
+
+}
+
